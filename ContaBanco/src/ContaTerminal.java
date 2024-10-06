5a57a82a6b2f9d3040ba1337abb46f673461a475
Phsadev <phsadev@hotmail.com>
2024-10-06 15:49:52 -0300

    Alterei a variável "valorSaldo = valorDeposito" para "valorSaldo +=valorDepósito".

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -15,13 +15,17 @@ public class ContaTerminal {
         System.out.println("\nDigite o nome do cliente: ");
         String nomeCliente = scanner.nextLine();
 
-        System.out.println("\nQual valordeseja depositar? ");
+        System.out.println("\nQual valor deseja depositar? ");
         double valorDeposito = scanner.nextDouble();
         scanner.nextLine();
 
-        double valorSaldo = valorDeposito;
+        double valorSaldo = 0;
+        
+        valorSaldo += valorDeposito;
 
-        System.out.println("\nOlá " + nomeCliente + " , obrigado por criar uma conta em nosso banco. Sua agência é a " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + valorSaldo + " já está disponível para saque.\n");
+        System.out.println("\nOlá " + nomeCliente + " , obrigado por criar uma conta em nosso banco. Sua agência é a " + numeroAgencia + ", conta " + numeroConta + " e seu saldo de " + valorSaldo + " já está disponível para saque.\n");
+        
+        scanner.close();
 
     }
 }
