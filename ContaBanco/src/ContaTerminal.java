508cdb5e7cd8009644e2e8a7e5f2516d89cca10d
Phsadev <phsadev@hotmail.com>
2024-10-06 00:04:09 -0300

    inclusão do valorSaldo recebendo valorDepósito.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nDigite o numero da sua conta: ");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("\nDigite o numero da agência: ");
+        String numeroAgencia = scanner.nextLine();
+
+        System.out.println("\nDigite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        double saldo = 1500.00;
+
+        System.out.println("\nOlá " + nomeCliente + " , obrigado por criar uma conta em nosso banco. Sua agência é a " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.\n");
+
     }
 }
